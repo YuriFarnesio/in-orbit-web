78b30684e2c37c8ba44c55ef09466f779a85a255
Yuri Farnesio <yuri.farnesio@gmail.com>
2024-09-15 13:01:40 -0300

    feat: use ui button

--- src/app.tsx
+++ src/app.tsx
@@ -1,6 +1,7 @@
 import { Plus } from 'lucide-react'
 import letsStart from './assets/lets-start-illustration.svg'
 import logo from './assets/logo-in-orbit.svg'
+import { Button } from './components/ui/button'
 
 export function App() {
   return (
@@ -10,13 +11,11 @@ export function App() {
       <p className="max-w-80 text-zinc-300 leading-relaxed text-center">
         Você ainda não cadastrou nenhuma meta, que tal cadastrar um agora mesmo?
       </p>
-      <button
-        type="button"
-        className="flex items-center bg-violet-500 hover:bg-violet-600 text-sm font-medium tracking-tight text-violet-50 gap-2 rounded-lg py-2.5 px-4 transition"
-      >
+
+      <Button>
         <Plus className="size-4" />
         Cadastrar meta
-      </button>
+      </Button>
     </div>
   )
 }
